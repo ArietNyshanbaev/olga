076dab612b9fb4ae4c98e6364206b1f043de428c
Ariet <YOUR@EMAIL.com>
2015-05-16 23:43:11 +0600

    Have finished signup

--- app/controllers/root_page_controller.rb
+++ app/controllers/root_page_controller.rb
@@ -1,4 +1,5 @@
 class RootPageController < ApplicationController
   def main
+  	@user = User.new
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new  
+  end
+
+  def create
+  	@user = User.new(user_params)
+
+  	name = @user.email.split('@') 
+    name = name[0].split('.')
+    first_name = name[0].capitalize
+    second_name = name[1].capitalize
+    name = first_name + " " + second_name
+    @user.name = name
+    
+    if @user.save
+      flash[:success] = "Yahooo!"
+      redirect_to @user # Handle a successful save.
+    else
+      render "root_page/main"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
