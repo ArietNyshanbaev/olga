f58aefd94586f24347a61cb3fc9a1b4d3847e949
Ariet <YOUR@EMAIL.com>
2015-05-18 04:30:09 +0600

    have fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,9 +2,16 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+%w( controller_one controller_two controller_three ).each do |controller|
+  Rails.application.config.assets.precompile += ["#{controller}.scss", "#{controller}.css"]
+end
+Rails.application.config.assets.precompile += %w( root_page.css )
+Rails.application.config.assets.precompile += %w( sessions.css )
+Rails.application.config.assets.precompile += %w( users.css )
