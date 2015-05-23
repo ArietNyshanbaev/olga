009a18a95a830312675bfa750f0d8791097f28f6
Ariet <YOUR@EMAIL.com>
2015-05-24 03:19:44 +0600

    password reset is added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,9 +15,8 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi there u have requested a password reset for this site if not just ignore this message"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'home_s' => 'home_second#main'
 
   get 'home' => 'home#main'
@@ -13,6 +17,7 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new'
   resources :users
   resources :account_activations, only: [:edit]  
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
